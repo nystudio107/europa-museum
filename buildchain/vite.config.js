8e3aa54bca6481fc8daca89f5e9069fa7d281d9b
Andrew Welch <andrew@keluli.local>
2021-10-19 20:04:52 -0400

    Remove Static Asset Fixer plugin, use server.origin in Vite ^2.6.0

--- buildchain/vite.config.js
+++ buildchain/vite.config.js
@@ -50,18 +50,6 @@ export default ({ command }) => ({
       ],
     }),
     vue(),
-    // Static Asset Fixer, see: https://github.com/vitejs/vite/issues/2394
-    {
-      name: 'static-asset-fixer',
-      enforce: 'post',
-      apply: 'serve',
-      transform: (code, id) => {
-        return {
-          code: code.replace(/\/src\/(.*)\.(svg|jp?g|png|webp)/g, 'http://localhost:3000/src/$1.$2'),
-          map: null,
-        }
-      },
-    },
   ],
   publicDir: '../src/public',
   resolve: {
@@ -70,6 +58,7 @@ export default ({ command }) => ({
     },
   },
   server: {
+    origin: 'http://localhost:3000/src/',
     host: '0.0.0.0',
   }
 });
